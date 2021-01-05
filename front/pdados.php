f791cc71ee231156ab897ca835f2c024b5954a32
ShoowTM <guilherme460@yahoo.com.br>
2021-01-05 18:40:28 -0300

    alteração css google
    Alterações no css para borda do google

--- front/pdados.php
+++ front/pdados.php
@@ -5,37 +5,43 @@ require_once('../inc/permissoes.php');
 ?>
 
 <!-- Begin Page Content -->
-<div class="container-fluid">
+<div class="container-fluid py-4">
 
   <!-- Page Heading -->
   <h1 class="h6 mb-6 text-gray-800">
     <a href="front.php?pagina=1"><i class="fas fa-home"></i> Home</a> /
-    <i class="fas fa-users"></i> Pesquisa
-    <span class="float-rigth"><a href="colaboradores.php?pagina=3" id="atualiz"><i class="fas fa-sync-alt"></i> Atualizar</a></span>
+    <i class="fab fa-google"></i> Pesquisa
+   <!--- <span class="float-rigth"><a href="colaboradores.php?pagina=3" id="atualiz"><i class="fas fa-sync-alt"></i> Atualizar</a></span> --->
   </h1>
-  <hr />
+  <hr/>
 
 <!-- Conteudo -->
  
 
         <!-- Topbar Search -->
 <div class="centro">
-        <img src="../img/busca.png">
-
+        <img src="../img/google.png">
+          <hr>
           <div class="dentro">
               
             <form class="container d-none d-sm-inline-block form-inline mr-auto ml-md-0 my-2 my-md-0 mw-100 navbar-search">
               <div class="input-group">
-               <input type="text" class="form-control bg-blue border-0 small" placeholder="Pesquisar" aria-label="Pesquisar" aria-describedby="basic-addon2">
+               <input type="text" class="borderradius form-control bg-blue border-0 small" placeholder="Pesquisar" aria-label="Pesquisar" aria-describedby="basic-addon2">
                 <div class="input-group-append">
-                 <button class="btn btn-dark" type="button">
-                    <i class="fas fa-search fa-sm"></i>
+                  <!--<button class="btn btn-dark" type="button"> 
+                    <i class="fas fa-search fa-sm"></i>-->
                   </button>
                 </div>
               </div>
             </form>
+            <hr>
+            
           </div>
-         
+         <div>
+          <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-sm text-white-50"></i> Buscar</a>
+         </div>
+
+
 
         <!-- Conteudo  /.container-fluid -->
 
