fd9dc37cfa531f0e6d33740173dca1f5f3bdfece
Felipe Lara <69820156+FelipeLaras@users.noreply.github.com>
2021-01-05 15:37:37 -0300

    Update termo.php

--- front/termo.php
+++ front/termo.php
@@ -5,12 +5,28 @@ require_once('header.php');
 <!-- Begin Page Content -->
 <div class="container-fluid">
   <!-- Page Heading -->
-  <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
-</div>
-<!-- /.container-fluid -->
+  <h1 class="h6 mb-6 text-gray-800">
+    <a href="front.php?pagina=1"><i class="fas fa-home"></i> Home</a> /
+    <i class="fas fa-users"></i> Colaboradores
+  </h1>
+  <hr />
+  <div class="text-center">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Colaborador <span class="colorRed">DESATIVADO!</span></h1>
+    <div style="display: <?= $_SESSION["ativar_cpf"] == 0 ? "none" : "block" ?>;">
+      <p class="mb-4">Deseja ativar ele novamente ?</p>
+      <a href="../inc/ativarfuncionario.php?id=<?= $_GET['id'] ?>" class="btn btn-success">SIM</a>
+    </div>
+    <div style="display: <?= $_SESSION["ativar_cpf"] == 0 ? "block" : "none" ?>;">
+      <p class="mb-4">Você não tem permissão para reativá-lo</p>
+      <a href="colaboradores.php?pagina=3" class="btn btn-warning">Voltar</a>
+    </div>
+  </div>
+  <!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
+
 <!-- Footer -->
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
