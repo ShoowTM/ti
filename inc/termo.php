47a6a2d1d1a0b8a792ada3310068d760abef2da5
Felipe Lara <69820156+FelipeLaras@users.noreply.github.com>
2021-01-06 11:24:29 -0300

    New

--- inc/termo.php
+++ inc/termo.php
@@ -1,20 +1,51 @@
 <?php
 session_start();
 
-require_once('../bd/conexao.php');
-
-if($_GET['id'] != NULL){
-
+if(empty($_GET['id'])){
+    $id_fun = $_SESSION['id_funcionario'];
 }else{
-
+    $id_fun = $_GET['id'];
 }
 
 
+if ($_POST['equip'] == NULL) {
+     //sim é todos os equipamentos
 
+     if ($_SESSION["perfil"] == 1) {
 
+        header('location: termopdf.php?id_fun='.$id_fun.'');
 
+    } else {        
 
+        header('location: termopdftecnicos.php?id_fun='.$id_fun.'');
+    }
 
+   
+} else {
+    
+     //não é todos os equipamentos
+     $equip = " WHERE MIE.id_equipamento in (";
 
-$conn->close();
-?>
+     $qte = count($_POST['equip']);
+ 
+     $cont = 1;
+ 
+     foreach ($_POST['equip'] as $id_equipamento) {
+         $equip .= $id_equipamento;
+ 
+         if ($qte == $cont) {
+             $equip .= ")";
+         } else {
+             $equip .= ",";
+         }
+ 
+         $cont++;
+     }
+ 
+     if ($_SESSION["perfil"] == 1) {
+         header('Location: termopdf.php?query=' . $equip . '');
+     } else {
+         header('Location: termopdftecnicos.php?query=' . $equip . '');
+     }
+   
+}
