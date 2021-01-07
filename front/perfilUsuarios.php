6beb5732681a8aee4caf803950bb64a7779a4758
Felipe Lara <69820156+FelipeLaras@users.noreply.github.com>
2021-01-07 10:36:49 -0300

    New

--- front/perfilUsuarios.php
+++ front/perfilUsuarios.php
@@ -1,17 +1,27 @@
 <?php
+/* ini_set('display_errors', 1);
+ini_set('display_startup_erros', 1);
+error_reporting(E_ALL); */
+session_start();
 require_once('header.php');
 require_once('../inc/dropdown.php');
 require_once('../inc/pesquisas.php');
+require_once('../bd/conexao.php');
 
 //permissão de tela
-if($_SESSION['perfil'] != '0'){
+if ($_SESSION['perfil'] != '0') {
   exit;
 }
 
 //pesquisa usuario sistemas
-$queryUsuarios .= " WHERE MP.id_profile = ".$_GET['id_usuario']."";
-$resultUsuario = $conn->query($queryUsuarios);
-$usuario = $resultUsuario->fetch_assoc()
+$queryUsuarios .= " WHERE MP.id_profile = " . $_GET['id_usuario'] . "";
+if (!$resultUsuario = $conn->query($queryUsuarios)) {
+  printf("Error[1] = %s\n", $conn->error);
+} else {
+  $usuario = $resultUsuario->fetch_assoc();
+}
+
+
 ?>
 <!-- Begin Page Content -->
 <div class="container-fluid">
@@ -23,91 +33,91 @@ $usuario = $resultUsuario->fetch_assoc()
     <i class="fas fa-pencil-alt"></i> Editando - <?= $usuario['profile_name'] ?>
   </h1>
   <hr />
-<!-- /.container-fluid -->
-<div class="col-lg-6 left">
-  <!-- Circle Buttons -->
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">Edição de perfil</h6>
-    </div>
-    <div class="card-body">
-      <form action="../inc/editarPerfil.php" method="POST">
-        <div class="form-group">
-          <label for="nome">Nome</label>
-          <input type="text" class="form-control" id="nome" value="<?= $usuario['profile_name'] ?>" name="nome">
-        </div>
-        <div class="form-group">
-          <label for="email">E-mail</label>
-          <input type="text" class="form-control" id="email" value="<?= $usuario['profile_mail'] ?>" name="email">
-        </div>
-        <div class="form-group">
-          <label for="senha">Senha</label>
-          <input type="password" class="form-control" id="senha" name="senha">
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlSelect2">Perfil</label>
-          <select class="form-control" id="exampleFormControlSelect2" name="perfil">
-            <option selected value="<?= $usuario["id_perfil"] ?>"><?= $usuario["nome_perfil"] ?></option>
-            <option>----------</option>
-            <?php
-            $resultPerfil = $conn->query($queryPerfil);
+  <!-- /.container-fluid -->
+  <div class="col-lg-6 left">
+    <!-- Circle Buttons -->
+    <div class="card shadow mb-4">
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Edição de perfil</h6>
+      </div>
+      <div class="card-body">
+        <form action="../inc/editarPerfil.php" method="POST">
+          <div class="form-group">
+            <label for="nome">Nome</label>
+            <input type="text" class="form-control" id="nome" value="<?= $usuario['profile_name'] ?>" name="nome">
+          </div>
+          <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="text" class="form-control" id="email" value="<?= $usuario['profile_mail'] ?>" name="email">
+          </div>
+          <div class="form-group">
+            <label for="senha">Senha</label>
+            <input type="password" class="form-control" id="senha" name="senha">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect2">Perfil</label>
+            <select class="form-control" id="exampleFormControlSelect2" name="perfil">
+              <option selected value="<?= $usuario["id_perfil"] ?>"><?= $usuario["nome_perfil"] ?></option>
+              <option>----------</option>
+              <?php
+              $resultPerfil = $conn->query($queryPerfil);
 
-            while ($perfil = $resultPerfil->fetch_assoc()) {
-              echo '<option value="' . $perfil['type_profile'] . '">' . $perfil['type_name'] . '</option>';
-            }
-            ?>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlInput1">Permissões</label>
-          <div class="form-group border-left-primary border-bottom-primary paddingDez">
-            <input type="text" style="display: none;" value="<?= $_GET['id_usuario'] ?>" name="id_usuario"> 
-            <div class="form-check ">
-              <input class="form-check-input" type="checkbox"  id="ativarCPF" <?= $usuario["ativar_cpf"] == 0 ?: "checked" ?> name="ativarCPF">
-              <label class="form-check-label" for="ativarCPF">
-                Ativar CPF
-              </label>
+              while ($perfil = $resultPerfil->fetch_assoc()) {
+                echo '<option value="' . $perfil['type_profile'] . '">' . $perfil['type_name'] . '</option>';
+              }
+              ?>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Permissões</label>
+            <div class="form-group border-left-primary border-bottom-primary paddingDez">
+              <input type="text" style="display: none;" value="<?= $_GET['id_usuario'] ?>" name="id_usuario">
+              <div class="form-check ">
+                <input class="form-check-input" type="checkbox" id="ativarCPF" <?= $usuario["ativar_cpf"] == 0 ?: "checked" ?> name="ativarCPF">
+                <label class="form-check-label" for="ativarCPF">
+                  Ativar CPF
+                </label>
+              </div>
+              <div class="form-check ">
+                <input class="form-check-input" type="checkbox" id="desativarCPF" <?= $usuario["desativar_cpf"] == 0 ?: "checked" ?> name="desativarCPF">
+                <label class="form-check-label" for="desativarCPF">
+                  Desativar CPF
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="emitir" <?= $usuario["emitir_check_list"] == 0 ?: "checked" ?> name="emitir">
+                <label class="form-check-label" for="emitir">
+                  Emitir Check-List
+                </label>
+              </div>
+              <div class="form-check ">
+                <input class="form-check-input" type="checkbox" id="editarHistorico" <?= $usuario["editar_historico"] == 0 ?: "checked" ?> name="editarHistorico">
+                <label class="form-check-label" for="editarHistorico">
+                  Editar histórico
+                </label>
+              </div>
+              <div class="form-check ">
+                <input class="form-check-input" type="checkbox" id="EditarFuncionario" <?= $usuario["editar_cadastro_funcionario"] == 0 ?: "checked" ?> name="EditarFuncionario">
+                <label class="form-check-label" for="EditarFuncionario">
+                  Editar Cadastro Funcionário
+                </label>
+              </div>
             </div>
-            <div class="form-check ">
-              <input class="form-check-input" type="checkbox" id="desativarCPF" <?= $usuario["desativar_cpf"] == 0 ?: "checked" ?> name="desativarCPF">
-              <label class="form-check-label" for="desativarCPF">
-                Desativar CPF
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="emitir" <?= $usuario["emitir_check_list"] == 0 ?: "checked" ?> name="emitir">
-              <label class="form-check-label" for="emitir">
-                Emitir Check-List
-              </label>
-            </div>
-            <div class="form-check ">
-              <input class="form-check-input" type="checkbox" id="editarHistorico" <?= $usuario["editar_historico"] == 0 ?: "checked" ?> name="editarHistorico">
-              <label class="form-check-label" for="editarHistorico">
-                Editar histórico
-              </label>
-            </div>
-            <div class="form-check ">
-              <input class="form-check-input" type="checkbox" id="EditarFuncionario" <?= $usuario["editar_cadastro_funcionario"] == 0 ?: "checked" ?> name="EditarFuncionario">
-              <label class="form-check-label" for="EditarFuncionario">
-                Editar Cadastro Funcionário
-              </label>
+
+          </div>
+          <button type="submit" class="btn btn-facebook btn-block">Salvar</button>
+          <div class="col-lg-15 mb-4 my-2 text-center" id="senha" style="display: <?= $_GET['msn'] == 1 ? "block" : "none" ?>;">
+            <div class="card bg-success text-white shadow">
+              <div class="card-body">
+                Salvo com sucesso!<br>
+                Para aplicar as alterações sair e entre novamente no sistema!
+              </div>
             </div>
           </div>
-
-        </div>
-        <button type="submit" class="btn btn-facebook btn-block">Salvar</button>
-        <div class="col-lg-15 mb-4 my-2 text-center" id="senha" style="display: <?= $_GET['msn'] == 1 ? "block" : "none" ?>;">
-          <div class="card bg-success text-white shadow">
-            <div class="card-body">
-              Salvo com sucesso!<br>
-              Para aplicar as alterações sair e entre novamente no sistema!
-            </div>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
-</div>
 
 </div>
 <!-- End of Main Content -->
--- inc/checkpdf.php
+++ inc/checkpdf.php
@@ -8,23 +8,23 @@ require_once('pesquisas.php');
 
 //EQUIPAMENTO
 if ($_GET['id_fun'] == NULL) {
-    $queryEquipamento .= $_GET['query'];
-    $funcionario = $_GET['query'];
+	$queryEquipamento .= $_GET['query'];
+	$funcionario = $_GET['query'];
 } else {
-    $queryEquipamento .= " WHERE MIE.id_funcionario = " . $_GET['id_fun'] . "";
-    $funcionario = " WHERE MIF.id_funcionario = " . $_GET['id_fun'] . "";
+	$queryEquipamento .= " WHERE MIE.id_funcionario = " . $_GET['id_fun'] . "";
+	$funcionario = " WHERE MIF.id_funcionario = " . $_GET['id_fun'] . "";
 }
 
 $resultEquip = $conn->query($queryEquipamento);
 
 
 //FUNCIONARIO
-$queryColaboradorPDF .= $funcionario." LIMIT 1";
+$queryColaboradorPDF .= $funcionario . " LIMIT 1";
 $resultColaborador = $conn->query($queryColaboradorPDF);
 $colaborador = $resultColaborador->fetch_assoc();
 
 //OBSERVAÇÕES
-$queryOBS = "SELECT * FROM manager.manager_inventario_obs WHERE id_funcionario = ".$colaborador['id_funcionario']." ORDER BY id_obs DESC LIMIT 1";
+$queryOBS = "SELECT * FROM manager.manager_inventario_obs WHERE id_funcionario = " . $colaborador['id_funcionario'] . " ORDER BY id_obs DESC LIMIT 1";
 $resultOBS = $conn->query($queryOBS);
 $obs = $resultOBS->fetch_assoc();
 
@@ -33,122 +33,167 @@ $html = "
 <html>
 	<head>
 		<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
-		<style type='text/css'>
-		p.termo_titulo{
-			 font-size: 10px; 
-			 font-weight: bold;
-		}
-		p.texto{
-			font-size:12px;
-		}
-		.titulo_segundario{
-			font-weight: bold;
-			font-size: 12px;
-			text-decaration: sublime
-		}
-		table{
-			font-size: 8px;
-		}
-		
-		th{
-			font-weight: bold;
-		}
-		</style>
+		<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' integrity='sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf' crossorigin='anonymous'>
+		<link rel='stylesheet' href='../css/checklistuser.css' crossorigin='anonymous'>	
 	</head>
 	<body>
-		<header>
-			<img id='logo' src='../img/logo.png' width='150' alt='Logo'>
-		</header>
-		<div id='termo_header'>
-			<p class='text-center'>&ldquo;TERMO DE ENTREGA E DECLARAÇÃO&rdquo;</p>
+
+		<div id='logo2'>
+			<img class='logo2' src='../img/logo2.png' width='130' alt='Logo'>
 		</div>
-		<div id='termo_body'>
-			<div id='termo_equipamento'>
-				<p class='text-center termo_titulo'>EQUIPAMENTOS CORPORATIVOS</p>
-			</div>
-			<div id='text_departamento'>
-				<span id='empresa_departamento'>
-					<p class='texto'>Na condição de empregado(a) da filial <span class='titulo_segundario'>" . $colaborador['empresa'] . " - " . $colaborador['departamento'] . " / " . $colaborador['funcao'] . "</span>, estou recebendo neste ato equipamento conforme abaixo:
-				</span>
-			</div>
+
+		<div id='logo'>
+			<img class='logo' src='../img/logo.png' width='150' alt='Logo'>
+		</div>
+
+		<div id='tabela'>
+			<table class='table table-sm'>
+				<thead>
+					<tr>
+						<th colspan='4'><p class='text-left'><u>1) Dados do Funcionário</u></p></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class='linha_table' scope='row'>FUNCIONÁRIO:</td>
+						<td class='info_user'>" . $colaborador['nome'] . "</td>
+						<td class='linha_table'>CPF:</td>
+						<td class='info_user'>" . $colaborador['cpf'] . "</td>
+					</tr>
+					<tr>
+						<td class='linha_table'>DEPARTAMENTO:</td>
+						<td class='info_user'>" . $colaborador['departamento'] . "</td>
+						<td class='linha_table'>FUNÇÃO:</td>
+						<td class='info_user'>" . $colaborador['funcao'] . "</td>
+					</tr>
+					<tr>
+						<td class='linha_table'>FILIAL:</td>
+						<td colspan='4' class='info_user'>" . $colaborador['empresa'] . "</td>
+					</tr>
+				<tbody>
+			</table>
+		</div>
+
+		<div id='equipamento_title'>
+			<p class='text-left'><u>2) Lista dos Equipamentos:</u></p>
+		</div>";
+
+while ($equipamento = $resultEquip->fetch_assoc()) {
+
+	//rowspan
+	if($equipamento['tipo_equipamento'] == 2 || $equipamento['tipo_equipamento'] == 1){
+		$rowspan = '6';
+	}else{
+		$rowspan = '2';
+	}
+
+	//acessorios
+	$queryAcessorios = "SELECT MDA.nome FROM manager_inventario_acessorios MIA LEFT JOIN manager_dropacessorios MDA ON (MIA.tipo_acessorio = MDA.id_acessorio) WHERE MIA.id_equipamento = " . $equipamento['id_equipamento'] . "";
+	$resultAcessorios = $conn->query($queryAcessorios);
+
+	$html .= "
 			<div id='tabela_equipamento'>
-				<div id='tabela_titulo_principal'>
-					<p class='titulo_segundario'><u>Descrição dos Produtos:</u></p>
-				</div>
-				<div id='termo_tabela'>
-				<!--SE FOR CHIP, MOSTRAR APENAS CHIP E OPERADORA-->
-					<table class='table table-sm'>
-					  <tr>
-						<th>EQUIP.</th>						
-						<th>NÚMERO</th>
-						<th>MODELO</th>
-						<th>PATRIMÔNIO</th>
-					    <th>IMEI</th> 
-					    <th>VALOR</th>
-					    <th>PLANOS</th>
-					    <th>ACESSÓRIOS</th>
-						<th>SITUAÇÃO</th>
-						<th>ESTADO</th>		    
-                      </tr>";
-					  while ($equipamento = $resultEquip->fetch_assoc()) {
+				" . $equipamento['tipo_equipamento'] . " - " . $equipamento['situacao'] . "
+				<table class='table table-sm'>
+					<tbody>
+						<tr>
+							<td rowspan='" . $rowspan . "' class='marcador' width='30'>(&nbsp; &nbsp;)</td>
+							<td colspan=''><b>Modelo:</b> " . $equipamento['modelo'] . "</td>
+							<td><b>Operadora:</b> " . $equipamento['operadora'] . "</td>												
+							<td rowspan='" . $rowspan . "' width='200'><u>OBS.:</u></td>
+						</tr>
+						<tr>
+							<td><b>Imei:</b> " . $equipamento['imei_chip'] . "</td>
+							<td><b>Numero:</b> " . $equipamento['numero'] . "</td>
+						</tr>
+						<tr>";
+	while ($acessorios = $resultAcessorios->fetch_assoc()) {
+		$html .= "<td colspan='2'>(&nbsp; &nbsp;)" . $acessorios['nome'] . "</td>";
+	}
+	$html .= "	</tr>
+					</tbody>
+				</table>
+			</div>";
+}
 
-							//acessorios
-							$queryAcessorios = "SELECT MDA.nome FROM manager.manager_inventario_acessorios MIA LEFT JOIN manager_dropacessorios MDA ON (MIA.tipo_acessorio = MDA.id_acessorio) WHERE MIA.id_equipamento = ".$equipamento['id_equipamento']."";
-							$resultAcessorios = $conn->query($queryAcessorios);
-						 
-							
-						  $html .= "<tr>";
-							  $html .= "<td>"; $html .= empty($equipamento['tipo_equipamento']) ? "---" : $equipamento['tipo_equipamento']; $html .= "</td>";
-							  $html .= "<td>"; $html .= empty($equipamento['numero']) ? "---" : $equipamento['numero']; $html .= "</td>";
-							  $html .= "<td>"; $html .= empty($equipamento['modelo']) ? "---" : $equipamento['modelo']; $html .= "</td>";
-							  $html .= "<td>"; $html .= empty($equipamento['patrimonio']) ? "---" : $equipamento['patrimonio']; $html .= "</td>";
-							  $html .= "<td>"; $html .= empty($equipamento['imei_chip']) ? "---" : $equipamento['imei_chip']; $html .= "</td>";
-							  $html .= "<td>"; $html .= empty($equipamento['valor']) ? "---" : $equipamento['valor']; $html .= "</td>";
-							  $html .= "<td>"; $html .= empty($equipamento['planos_voz']) || empty($equipamento['planos_dados']) ? "---" : $equipamento['planos_dados'].",". $equipamento['planos_voz']; $html .= "</td>";
-							  
-							  $html .= "<td>"; 
-							  
-							  while($acessorios = $resultAcessorios->fetch_assoc() ){
-								$html .= $acessorios['nome']."-";
-							  }
-							  
-							  $html .= "</td>";
-							  
-							  $html .= "<td>"; $html .= empty($equipamento['situacao']) ? "---" : $equipamento['situacao']; $html .= "</td>";
-							  $html .= "<td>"; $html .= empty($equipamento['estado']) ? "---" : $equipamento['estado']; $html .= "</td>";						
-						  $html .= "</tr>";
-					  }
-		  $html .= "</table>
-				</div>
-			</div>
-			<div id='termo_texto'>
-				<p class='text-sm-left texto'>Comprometendo-me a devolvê-lo, em perfeito estado de conservação, mediante simples solicitação da empresa ou no caso de rescisão contratual, independente do motivo. Declaro que a utilização do referido equipamento <span class='font-weight-bold'><u> será exclusivamente em minha atividade profissional</u></span>,(não esta autorizado fotos particulares, telefones particulares, redes sociais, facebook, instagram, tinder, badoo, happn) estou ciente, da minha responsabilidade por danificar culposamente(pelo extravio, queda, danos por contato com umidade, extravio de componentes(carregador), estando isento de responsabilidade por danos advindos de desgates natural por uso cotidiano). Ciente que em caso de mau funcionamento (bateria não carrega) ou defeito do aparelho devo notificar no prazo máximo de 3 dias, após a retirada.</p>
-				<p class='text-sm-left texto'>Caso haja necessidade de portar, levar para casa este eletrônico, que devo notificar qualquer estrago ou avaria imediatamente a para área de T.I, que caso se faça necessários consertos, estes deverão preferencialmente ser realizados via T.I, só em caso de impossibilidade incompatibilidade (devido a distância) o conserto será feito de forma particular.
+$html .= "<p class='text-left'>Na condição de empregado(a) da filial <b>" . $colaborador['empresa'] . "</b>, estou devolvendo neste ato os equipamentos descritos conforme a cima.</p>
+		<p class='text-left'><u>3) CHECAR ITENS NA DEVOLUÇÃO</u></p>
+		<div id='tabela_devolucao'>
 
-					Para os fins do par. 1º do Art. 462 da CLT, desde já autorizo o desconto salarial à conta de eventuais danos
-					causados ao equipamento,(descritos acima) reembolsando a minha empregadora pelos reparos necessários ou até mesmo a substituição de um novo aparelho. Lembrando que o valor para ressarcimento será o vigente da data da ocorrência.<p>
-			</div>
-			<div id='tabela_titulo_principal'>
-				<p class='titulo_segundario'><u>Observações:</u></p>
-			</div>
-			<div id='termo_texto'>";
-			if ($obs['obs'] != NULL) {
+			<table style='font-size: 9px; border: 1px solid #dee2e6'>
+			  <thead>
+			    <tr>
+			      <th>Checar</th>
+			      <th style='padding-right: 6px;border: solid 1px #dee2e6;'>Status</th>
+			      <th style='padding-right: 300px;'>Observacao + Valor do Conserto</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <tr>
+			      <td>Senha de Desbloqueio</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Dados Pessoais</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Carregador</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Fone de Ouvido</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Cabos</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Botões Faltantes</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Tela Trincada</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Tela Trincada</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Danos por Queda</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			    <tr>
+			      <td>Umidade</td>
+			      <td></td>
+			      <td></td>
+			    </tr>
+			  </tbody>
+			</table>
 
-				$html .= "<div id='termo_texto'>
-								<p class='text-sm-left texto'>&raquo; " . $obs['obs'] . "</p>
-							</div>";
-			}
-			
-			$html .= "</div>
-			<br>
-			<div id='termo_data'>
-				<p class='text-center'>____________, ____ de _____________ de ________</p>
-			</div>
-			<div id='termo_footer'>
-				<p class='font-weight-light'>Colaborador(A): " . $colaborador['nome'] . "</p>
-				<p class='font-weight-light'>CPF: " . $colaborador['cpf'] . "</p>
-				<p class='font-weight-light'>Assinatura:_______________________________________________________________ </p>
-			</div>
+		</div>
+		<div style='margin-top: 10px'>
+			<p class='text-left'>Para os fins do par. 1° do Art. 462 a CLT, desde já autorizo o desconto nas minhas verbas rescisórias, afim de ressarcir os danos acima.</p>
+		</div>
+		<div id='termo_data'>
+			<p class='text-center'>____________, ____ de _____________ de ________</p>
+		</div>
+		<div id='assinatura'>
+			<p class='text-left'>______________________________</p>
+			<p class='text-left' style='margin-top: -18px;'>" . $colaborador['empresa'] . "</p>
+			<p class='text-left'>______________________________</p>
+			<p class='text-left' style='margin-top: -18px;'>" . $colaborador['nome'] . "</p>
 		</div>
 	</body>
 </html>";
@@ -166,7 +211,6 @@ use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
-
 $dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation
@@ -176,9 +220,7 @@ $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
 
 // Output the generated PDF to Browser
+$dompdf->stream('checklist_' . $colaborador['nome'] . '.pdf', array("Attachment" => 1)); //1 - Download 0 - Previa
 
-$dompdf->stream('termo_' . $colaborador['nome'] . '.pdf', array("Attachment" => 1));//1 - Download 0 - Previa
-
-
+//close dados bank
 $conn->close();
-?>
